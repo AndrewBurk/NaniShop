3496512b0fddd06dcfb3f9034e0f77c2ec3f0dd5
andrewburk <burkmag@gmail.com>
2016-03-09 22:33:41 +0200

    Fixed issue with reloading app on product-details.html page. Now content for single product is stored in LocalStorage.

--- client/public/app/Shop/controller.js
+++ client/public/app/Shop/controller.js
@@ -2,13 +2,13 @@
  * Created by andrew on 08.03.16.
  */
 function storeController($scope, $stateParams, DataService){
-    console.log('dataservice');
     $scope.cart = DataService.cart;
     $scope.store = DataService.store;
 
     if ($stateParams.item_id != null) {
-        $scope.product = $scope.store.getProduct($stateParams.item_id);
-        console.log('getItemId');
+        var product = $scope.store.getProduct($stateParams.item_id);
+        localStorage['products'] = product != null ? JSON.stringify(product) : localStorage['products'];
+        $scope.product = JSON.parse(localStorage['products']);
     }
 
 }
